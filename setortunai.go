abcf468f64347a48179a2dc78eea4ac13a744afa
ShinyQ_11 <kurniadi_wijaya_25rpl@student.smktelkom-mlg.sch.id>
2019-12-02 23:30:19 +0700

    ADD | Done Case Last Transaction 8 Month Ago

--- setortunai.go
+++ setortunai.go
@@ -11,11 +11,15 @@ type TNasabah struct {
 	nik, nama, norek, jenis          string
 	pin, saldo, nominalTerakhir      int
 	tanggalDaftar, transaksiTerakhir string
+	bulanTransaksi, tahunTransaksi   int
 }
 
 var TArrNasabah []TNasabah
 var pilih int
 var waktu = time.Now().Format("2006-01-02")
+var waktuN = time.Now()
+var tahun = waktuN.Year()
+var bulan = int(waktuN.Month())
 
 func cariRekening(jenisRek string) int {
 	count := 0
@@ -45,11 +49,11 @@ func cari_nik(nik string) int {
 
 func registrasi() {
 	var (
-		nik, nama, norek, jenisRek                  string
-		pin, jenis, saldo, nominalTerakhir          int
-		tanggalDaftar, transaksiTerakhir            string
-		validnik, validSetoran, validJenis, Selesai bool
-		inputLagi                                   string
+		nik, nama, norek, jenisRek                                         string
+		pin, jenis, saldo, nominalTerakhir, tahunTransaksi, bulanTransaksi int
+		tanggalDaftar, transaksiTerakhir                                   string
+		validnik, validSetoran, validJenis, Selesai                        bool
+		inputLagi                                                          string
 	)
 
 	for !Selesai {
@@ -116,10 +120,14 @@ func registrasi() {
 		pin = 100000 + rand.Intn(999999-100000+1)
 		index := len(TArrNasabah)
 
+		tahunTransaksi = tahun
+		bulanTransaksi = bulan
+
 		nasabah := TNasabah{
 			nik, nama, norek, jenisRek, pin,
 			saldo, nominalTerakhir,
 			tanggalDaftar, transaksiTerakhir,
+			tahunTransaksi, bulanTransaksi,
 		}
 		TArrNasabah = append(TArrNasabah, nasabah)
 
@@ -234,6 +242,8 @@ func nasabah_setoran(index int) {
 		TArrNasabah[index].saldo += setoran
 		TArrNasabah[index].transaksiTerakhir = waktu
 		TArrNasabah[index].nominalTerakhir = setoran
+		TArrNasabah[index].bulanTransaksi = bulan
+		TArrNasabah[index].tahunTransaksi = tahun
 
 		fmt.Println("\nSukses Menambahkan Setoran Sebesar", setoran, "\n")
 
@@ -384,7 +394,7 @@ func nasabah_jenis_rekening() {
 
 	for !validJenis {
 		fmt.Print("Masukkan Jenis Rekening (1/2/3): ")
-
+		fmt.Scanln(&jenis)
 		if jenis == 1 || jenis == 2 || jenis == 3 {
 			validJenis = true
 		} else {
@@ -433,9 +443,9 @@ func nasabah_jenis_rekening() {
 				jumlah--
 			}
 
-			fmt.Println("\nDitemukan : ", len(sortNasabah), "Data Nasabah Dengan Jenis Rekening", sortNasabah[0].jenis)
+			fmt.Println("\nDitemukan : ", len(sortNasabah), "Data Nasabah Dengan Jenis Rekening", sortNasabah[0].jenis, "\n")
 			for i := 0; i < len(sortNasabah); i++ {
-				fmt.Println("NIK :", sortNasabah[i].nik)
+				fmt.Println("\nNIK :", sortNasabah[i].nik)
 				fmt.Println("Nama :", sortNasabah[i].nama)
 				fmt.Println("PIN :", sortNasabah[i].pin)
 				fmt.Println("Nomor Rekening :", sortNasabah[i].norek)
@@ -445,11 +455,53 @@ func nasabah_jenis_rekening() {
 				fmt.Println("Nominal Transaksi Terahir :", sortNasabah[i].nominalTerakhir)
 			}
 		} else {
-			fmt.Println("Data Nasabah Dengan Jenis Rekening", jenis, "Tidak Ditemukan")
+			fmt.Println("Data Nasabah Dengan Jenis Rekening", jenisTab, "Tidak Ditemukan")
 		}
 
 	} else {
 		fmt.Println("Belum Ada Data Nasabah")
+	}
+
+	menu_manager()
+}
+
+func tidak_transaksi() {
+	var TSortTranksaksi []TNasabah
+
+	if len(TArrNasabah) != 0 {
+		for i := 0; i < len(TArrNasabah); i++ {
+			if (tahun-TArrNasabah[i].tahunTransaksi == 0 && bulan-TArrNasabah[i].bulanTransaksi >= 8) || (tahun-TArrNasabah[i].tahunTransaksi == 1 && bulan-TArrNasabah[i].bulanTransaksi <= 4) || (tahun-TArrNasabah[i].tahunTransaksi >= 2) {
+				nasabah := TNasabah{
+					nama:              TArrNasabah[i].nama,
+					jenis:             TArrNasabah[i].jenis,
+					nik:               TArrNasabah[i].nik,
+					nominalTerakhir:   TArrNasabah[i].nominalTerakhir,
+					norek:             TArrNasabah[i].norek,
+					pin:               TArrNasabah[i].pin,
+					saldo:             TArrNasabah[i].saldo,
+					tanggalDaftar:     TArrNasabah[i].tanggalDaftar,
+					transaksiTerakhir: TArrNasabah[i].transaksiTerakhir,
+				}
+				TSortTranksaksi = append(TSortTranksaksi, nasabah)
+			}
+		}
+	}
+
+	if len(TSortTranksaksi) != 0 {
+
+		fmt.Println("\nDitemukan : ", len(TSortTranksaksi), "Data Nasabah")
+		for i := 0; i < len(TSortTranksaksi); i++ {
+			fmt.Println("\nNIK :", TSortTranksaksi[i].nik)
+			fmt.Println("Nama :", TSortTranksaksi[i].nama)
+			fmt.Println("PIN :", TSortTranksaksi[i].pin)
+			fmt.Println("Nomor Rekening :", TSortTranksaksi[i].norek)
+			fmt.Println("Jenis ATM :", TSortTranksaksi[i].jenis)
+			fmt.Println("Setoran :", TSortTranksaksi[i].saldo)
+			fmt.Println("Transaksi Terahir :", TSortTranksaksi[i].transaksiTerakhir)
+			fmt.Println("Nominal Transaksi Terahir :", TSortTranksaksi[i].nominalTerakhir)
+		}
+	} else {
+		fmt.Println("Data Nasabah Tidak Ditemukan")
 	}
 
 	menu_manager()
@@ -491,7 +543,7 @@ func menu_manager() {
 	} else if pilih == 2 {
 		tampil_nasabah()
 	} else if pilih == 3 {
-		fmt.Print("DUMMY TIDAK MELAKUKAN TRANSAKSI")
+		tidak_transaksi()
 	} else if pilih == 4 {
 		nasabah_jenis_rekening()
 	} else if pilih == 5 {
@@ -533,8 +585,8 @@ func main() {
 
 func tambahData() {
 	TArrNasabah = append(TArrNasabah,
-		TNasabah{"1234567890123456", "Dony", "XYZ-S002", "Silver", 123456, 500000, 500000, "2019-11-25", "2019-11-25"},
-		TNasabah{"1234567890123455", "Aini", "XYZ-S001", "Silver", 123456, 500000, 500000, "2019-11-25", "2019-11-25"},
-		TNasabah{"1234567890123454", "Angela", "XYZ-G001", "Gold", 123456, 500000, 500000, "2019-11-25", "2019-11-25"},
+		TNasabah{"1234567890123456", "Dony", "XYZ-S002", "Silver", 123456, 500000, 500000, "2019-11-25", "2019-11-25", 5, 2018},
+		TNasabah{"1234567890123455", "Aini", "XYZ-S001", "Silver", 123456, 500000, 500000, "2019-11-25", "2019-11-25", 1, 2019},
+		TNasabah{"1234567890123454", "Angela", "XYZ-G001", "Gold", 123456, 500000, 500000, "2019-11-25", "2019-11-25", 5, 2018},
 	)
 }
